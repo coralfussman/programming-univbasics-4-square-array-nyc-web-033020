319ad1ec353f1c460aae44916eee29fda8fe562f
Coral Fussman <coralfussman@gmail.com>
2020-03-16 23:07:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+numbers = [1,2,3]
+counter = 0
+while counter < numbers.length do
+
+puts array[counter]
+  counter ** counter
+  counter +=1
+
+end
 end
