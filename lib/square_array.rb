fdd9bef92f2e11bced7565a62878cbd8b8dc3edd
Coral Fussman <coralfussman@gmail.com>
2020-03-16 23:37:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
-
+number =[]
 counter = 0
 while counter < array.length do
 
 puts array[counter]
-  counter +=1
-counter * counter
-
+number[0] +=1
+number * number
+counter +=1
 end
 end
 square_array([1,2,3])
